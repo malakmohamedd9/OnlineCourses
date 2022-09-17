ada34b8a4680ff407b6157426e514c874b8d14c8
Malak Mohamed <malakmohamedd9@gmail.com>
2022-09-17 14:27:43 +0200

    updates

--- server/config/dev.js
+++ server/config/dev.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb+srv://TM1:TM1@cluster0.7gj4kw9.mongodb.net/?retryWrites=true&w=majority",
+    mongoURI: "mongodb+srv://TM1:TM123@cluster0.slyhgxb.mongodb.net/?retryWrites=true&w=majority",
     secretOrKey: "TM2022"
 };
